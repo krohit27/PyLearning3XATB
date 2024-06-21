420be6b6a6c7e99e9f43e7c09c98420e879ab976
Rohit Kalshetty <krohit7545@gmail.com>
2024-06-21 22:46:06 +0530

    Decorator and Dict

--- Ex_21062024/Lab90.py
+++
@@ -1,2 +0,0 @@
-ENV_API_URLS = tuple(["abc.com/get", "xyz.com/post", "qwe.com/put"])
-print(ENV_API_URLS)
--- Ex_21062024/Lab93.py
+++
@@ -1,6 +0,0 @@
-set1 = {1, 2, 3, 4, 5}
-set2 = {2, 3, 9}
-subset = set2.issubset(set1)
-print(subset)
-# subset = set1.issubset(set2)
-# print(subset)
