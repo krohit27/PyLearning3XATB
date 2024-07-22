3ef352ac5ce8bfdf19df7b3a825d70272b16d2c0
Rohit Kalshetty <krohit7545@gmail.com>
2024-07-23 00:10:23 +0530

    Pytest

---
+++ Ex02_JULLY/22072024_API/test_Lab180.py
@@ -0,0 +1,9 @@
+
+def test_addition():
+    assert 1+1 == 2
+
+# Assert - assertion - what you want to validate ?
+# Actual Result == Expected Result
+
+def test_sub():
+        assert 5 - 3 == 9
---
+++ Ex02_JULLY/22072024_API/test_Lab181.py
@@ -0,0 +1,2 @@
+def test_sub():
+    assert 4 - 2 == 2
