7b5ea407a0f455429ad090357e4a50c9cb720c84
Rohit Kalshetty <krohit7545@gmail.com>
2024-07-27 15:16:54 +0530

    Fixtures

---
+++ Ex02_JULLY/26072024/test_Lab197.py
@@ -0,0 +1,16 @@
+import csv
+import pandas
+import pandas as pd
+
+# pip install pandas
+class Test_CRUD(object):
+    def test_update_1(self):
+        # Read the file
+        with open('ex02_July/26072024/userdata.csv') as csvfile:
+            reader= csv.reader(csvfile)
+            for row in reader:
+                print(row[0],row[1])
+
+    def test_update_2(self):
+        df = pd.read_csv('ex02_July/26072024/userdata.csv')
+        print(df)
